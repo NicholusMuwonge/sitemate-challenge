6138eca5f205a0ebcd6edaa2554b5eaed64ba275
Muwonge Nicholus <nicholusmuwonge@gmail.com>
2021-12-05 14:07:02 +0300

    Create class for confirmation dialog to make it reusable.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-const showDialog=()=>{
-    alert('I have been clicked.')
+class ConfirmationDialog {
+  #modalBox = document.createElement("div");
+  #overlay = document.createElement("div");
+  static modalContent = document.getElementById("modal-content");
+  constructor(message = "", yes = "Yes", cancel = "Cancel") {
+    this.message = message;
+    this.yes = yes;
+    this.cancel = cancel;
+  }
 }
+
+const firstDialog = new ConfirmationDialog("Are you there?");
+const secondDialog = new ConfirmationDialog(
+  "Are you sure yo want to continue?",
+  "Hell Yeah",
+  "Not so Much"
+);
+console.log(firstDialog);
